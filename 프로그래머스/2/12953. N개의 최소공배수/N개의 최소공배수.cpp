234774d79b3e70fa6f32cd1b8448ded73995e0c6
yukiys <150210372+yukiys@users.noreply.github.com>
2026-06-25 17:36:55 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+
+    for(int i = 1; i < arr.size(); i++)
+        answer = lcm(answer, arr[i]);
+
+    return answer;
+}
